11a4c367112f21a0ec1d0ebc2d1a6d2a46dbc1ea
shuzaei <shuzaei@gmail.com>
2021-06-14 13:21:30 +0900

    update: vr.cpp

--- vr.cpp
+++ vr.cpp
@@ -1,9 +1,29 @@
+#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
-namespace VR
+namespace VRManager
 {
+    class Data
+    {
+    private:
+        std::string name;
+
+    public:
+        std::string SetName(std::string newName)
+        {
+            std::string oldName = this->name;
+            this->name = newName;
+            return oldName;
+        }
+        std::string GetName()
+        {
+            return this->name;
+        }
+    };
+
     std::fstream manager;
 
     std::string Bronze(std::string string)
@@ -26,13 +46,13 @@ namespace VR
         return "\e[37;46m" + string + "\e[m";
     }
 
-    void Register(int argc, char **argv, std::ostream &ostream)
+    void Register(std::ostream &ostream, std::vector<std::string> command)
     {
         std::string log;
-        for (int i = 1; i < argc; i++)
+        for (int i = 1; i < (int)command.size(); i++)
         {
-            log += (std::string)argv[i];
-            if (i != argc - 1)
+            log += command[i];
+            if (i != (int)command.size() - 1)
             {
                 log += ' ';
             }
@@ -40,35 +60,89 @@ namespace VR
         ostream << log << std::endl;
     }
 
-    void VRUsage(std::ostream &ostream, std::string argv0)
+    // update
+    void PrintVRUsage(std::ostream &ostream)
     {
-        std::string usage = "Usage: " + argv0 + " <command>\n"
-                                                "Commands:\n";
+        static std::string usage = "Usage: vr <command>\n"
+                                   "Commands:\n";
         ostream << usage;
     }
 
-    bool ManagerOpen()
+    void PrintOpenManagerFailure(std::ostream &ostream)
+    {
+        static std::string failure = "VRManager::OpenManager: std::fstream&::open: cannot open file ~/.vrmanager\n";
+        ostream << failure;
+    }
+
+    void OpenManager()
     {
         std::ios_base::openmode openmode = std::ios_base::in | std::ios_base::out | std::ios_base::app;
         manager.open("~/.vrmanager", openmode);
-        return (bool)manager;
+        if ((bool)manager == false)
+        {
+            openmode = std::ios_base::out;
+            manager.open("~/.vrmanager", openmode);
+            if ((bool)manager == false)
+            {
+                VRManager::PrintOpenManagerFailure(std::cerr);
+                exit(1);
+            }
+        }
     }
 
-    void ParseCommand(int argc, char **argv)
+    std::vector<std::string> StringToCommand(std::string string)
     {
-        if (argc <= 1)
+        std::string::iterator leftIterator = string.begin(), rightIterator = string.end();
+        std::vector<std::string> command;
+
+        while (leftIterator != string.end())
         {
-            VRUsage(std::cerr, argv[0]);
+            command.emplace_back(std::string(leftIterator, std::find(leftIterator, rightIterator, ' ')));
+        }
+        return command;
+    }
+    std::vector<std::string> ArgToCommand(int argc, char **argv)
+    {
+        std::vector<std::string> command;
+
+        for (int i = 0; i < argc; i++)
+        {
+            command.emplace_back((std::string)argv[i]);
+        }
+        return command;
+    }
+
+    void Execute(std::vector<std::string> command)
+    {
+        if (command.size() <= 1)
+        {
+            PrintVRUsage(std::cerr);
             exit(1);
         }
         else
         {
-            std::cout << "Work in Progress." << std::endl;
+            if (command[1] == "init")
+            {
+            }
         }
+    }
+};
+
+namespace VR
+{
+    void Main(int argc, char **argv)
+    {
+        VRManager::OpenManager();
+        std::string command;
+        while (std::getline(VRManager::manager, command))
+        {
+            VRManager::Execute(VRManager::StringToCommand(command));
+        }
+        VRManager::Execute(VRManager::ArgToCommand(argc, argv));
     }
 };
 
 int main(int argc, char **argv)
 {
-    VR::ParseCommand(argc, argv);
+    VR::Main(argc, argv);
 }
