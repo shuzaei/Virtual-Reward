bc7dc74cd35813a3e50196bbcace7c22b854d96e
Shuz* <42332130+shuzaei@users.noreply.github.com>
2021-06-18 01:23:10 +0000

    fix: install.sh: use compile option -std=c++17

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo "Compiling vr.cpp ..."
-g++ vr.cpp -o vr 2> vr.log
+g++ vr.cpp -o vr -std=c++17 2> vr.log
 echo "Moving vr ..."
 sudo mv vr /usr/local/bin/
 echo -e "\e[32mInstallation Compreted\e[0m"
