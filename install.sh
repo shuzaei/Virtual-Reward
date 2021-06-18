e5dec947f2704c412699ac6b969328d00ddcb4af
Shuz* <42332130+shuzaei@users.noreply.github.com>
2021-06-18 01:39:30 +0000

    fix: install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,24 @@
-echo "Compiling vr.cpp ..."
+echo -e "Compiling vr.cpp ..."
+
+if [ ! -e "vr.cpp" ]; then
+    echo -e "\e[31mError:\e[0m cannot find \"vr.cpp\""
+    exit 1
+fi
+
 g++ vr.cpp -o vr -std=c++17 2> vr.log
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mError:\e[0m cannot compile vr.cpp"
+fi
+
 echo "Moving vr ..."
-sudo mv vr /usr/local/bin/
-echo -e "\e[32mInstallation Compreted\e[0m"
+if [ ! -e "vr" ]; then
+    echo -e "\e[31mError:\e[0m cannot find \"vr\""
+    exit 1
+fi
+sudo mv vr /usr/local/bin/ 2> vr.log
+
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mError:\e[0m cannot move vr into /usr/local/bin/"
+fi
+
+echo -e "\e[32mInstallation Completed\e[0m"
